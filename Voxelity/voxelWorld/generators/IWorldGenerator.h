20fe85f58c4a22e922d05421794c3a5f5d135d39
adrian <adrileroux9@gmail.com>
2025-05-19 00:07:00 +0200

    comments

--- Voxelity/voxelWorld/generators/IWorldGenerator.h
+++ Voxelity/voxelWorld/generators/IWorldGenerator.h
@@ -10,12 +10,36 @@
 struct ChunkData;
 class Chunk;
 
+/**
+ * @brief Abstract interface for chunk voxel data generators.
+ *
+ * Classes implementing this interface provide methods to generate voxel
+ * types for individual positions and to generate a full chunk's data.
+ */
 class IChunkGenerator {
 public:
     virtual ~IChunkGenerator() = default;
 
+    /**
+    * @brief Generate the voxel type at a given global position.
+    *
+    * This pure virtual method must be implemented to define how to
+    * determine the BlockType of a voxel at the specified global coordinates.
+    *
+    * @param globalPos The global 3D position in world coordinates.
+    * @return The BlockType for the voxel at the given position.
+    */
     virtual BlockType generateVoxel(const glm::ivec3& globalPos) = 0;
 
+    /**
+    * @brief Generate the full chunk data at the specified global chunk position.
+    *
+    * This method generates a complete ChunkData object by invoking generateVoxel
+    * for each voxel within the chunk bounds.
+    *
+    * @param globalChunkPos The global chunk coordinates identifying which chunk to generate.
+    * @return A ChunkData instance containing all voxel data for the chunk.
+    */
     ChunkData generate(glm::ivec3 globalChunkPos);
 };
 
--- Voxelity/voxelWorld/generators/NaturalWorldGenerator.h
+++ Voxelity/voxelWorld/generators/NaturalWorldGenerator.h
@@ -7,10 +7,29 @@
 
 #include "IWorldGenerator.h"
 
+/**
+ * @brief Concrete implementation of IChunkGenerator that generates natural terrain.
+ *
+ * This class provides voxel generation logic for natural world features such as
+ * ground, hills, and other terrain types by overriding the generateVoxel method.
+ */
 class NaturalWorldGenerator final : public IChunkGenerator {
 public:
+
+    /**
+    * @brief Default constructor for NaturalWorldGenerator.
+    */
     NaturalWorldGenerator();
 
+    /**
+    * @brief Generates the voxel type at the given world position.
+    *
+    * This method defines the procedural rules for terrain generation,
+    * returning the appropriate BlockType for the voxel at 'position'.
+    *
+    * @param position The 3D integer position in world coordinates to generate a voxel for.
+    * @return The BlockType corresponding to the voxel at the specified position.
+    */
     BlockType generateVoxel(const glm::ivec3& position) override;
 };
 
